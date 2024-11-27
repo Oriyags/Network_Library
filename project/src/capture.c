d2b8e5ba8fc5dbceef30db2946e26bbccc821579
Oriya <oriyas100@gmail.com>
2024-11-27 11:02:33 +0200

    Fixed loop and Buffer name

--- project/src/capture.c
+++ project/src/capture.c
@@ -12,7 +12,8 @@
 #define BUFFER_SIZE 65536       // Define a buffer size for storing captured packet data
 
 // Function to start capturing packets
-void start_capture() {
+void start_capture() 
+{
     // Create a raw socket to capture packets at the data link layer (Ethernet)
     int sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock_raw < 0) {         // Check if socket creation was successful
@@ -20,13 +21,16 @@ void start_capture() {
         exit(1);                // Exit program if socket cannot be created
     }
 
-    unsigned char buffer[BUFFER_SIZE]; // Buffer to store packet data
+    // [CR] buffer of what? how about packet_buffer?
+    unsigned char data_buffer[BUFFER_SIZE]; // Buffer to store packet data
     struct sockaddr saddr;             // Structure to store address of captured packet
     int saddr_len = sizeof(saddr);     // Initialize address length
 
-    while (1) {                        // Infinite loop to continuously capture packets
+    // Infinite loop to continuously capture packets
+    for (;;) 
+    {
         // Receive packets from the socket and store them in the buffer
-        int data_size = recvfrom(sock_raw, buffer, BUFFER_SIZE, 0, &saddr, (socklen_t*)&saddr_len);
+        int data_size = recvfrom(sock_raw, data_buffer, BUFFER_SIZE, 0, &saddr, (socklen_t*)&saddr_len);
         if (data_size < 0) {   // Check if packet reception was successful
             perror("Recvfrom error"); // Print error message if packet reception fails
             break;             // Exit loop on reception error
@@ -35,7 +39,7 @@ void start_capture() {
         printf("Packet captured! Size: %d bytes\n", data_size);
         
         // Call function to parse the captured Ethernet frame
-        parse_ethernet_frame(buffer);
+        parse_ethernet_frame(data_buffer);
     }
 
     close(sock_raw);           // Close the socket once capturing is complete
